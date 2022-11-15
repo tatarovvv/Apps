f35d36f8d087a519996e871f55ee204eb3024f99
Mennan <mennankrm@hotmail.com>
2022-11-15 14:28:51 +0100

    E1 tot 6

---
+++ SOM/lec1.py
@@ -0,0 +1,25 @@
+# Exercise 1
+def prod3(x1,x2,x3):
+    return (x1 * x2 * x3)
+
+# Exercise 2
+def square(x):
+    return x * x
+def quadProd3(x1,x2,x3):
+    print(prod3(square(x1), square(x2), square(x3)))
+
+# Exercise 3 
+def BMI(weight, height):
+   print( weight / height **2 )
+
+
+# Exercise 6
+
+def compareLengths(string1,string2):
+    if len(string1) == len(string2):
+        print("True")
+    else: print ("False")
+
+# Exercise 7
+
+sum()
