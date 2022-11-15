98e2471db611d87b63d5de0f9da5b838c3bfb74a
tatarovvv <mennankrm@hotmail.com>
2022-11-15 17:52:15 +0100

    Lec 1 done

--- SOM/lec1.py
+++ SOM/lec1.py
@@ -20,6 +20,61 @@ def compareLengths(string1,string2):
         print("True")
     else: print ("False")
 
-# Exercise 7
+# Exercise 7 & 8 & 20
 
-sum() #test0
+def getSum(n):
+ 
+    sum = 0
+    while (n != 0):
+ 
+        sum = sum + int(n % 10)
+        n = int(n/10)
+    
+    return sum
+
+def countDigits(x):
+    print(getSum(x))
+
+# Exercise 10
+
+def alpha(x):
+    if len(x) == 1:
+        if x.isnumeric(): print("1")
+        else: print("0")
+        if x.isalnum() == False: print("-1")
+    else: print("Error! Max 1 parameter")
+
+# Exercise 21
+
+def lowerCaseOnly(x):
+    for c in x:
+     if c.islower():
+         print (c)
+
+# Exercise 26        
+
+def isPrime(n):
+  for i in range(2, n):
+        if n % i == 0:
+            return False
+        return True
+
+def primos(n):
+    for i in range(2, n + 1):
+        if isPrime(i):
+            print(i, end = " ")
+
+     
+
+
+    
+
+
+
+
+ 
+    
+
+
+        
+
