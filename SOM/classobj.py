7677ab7bcc7d1ffb073a05f3d24826352f4f76df
Mennan <mennankrm@hotmail.com>
2022-11-21 12:37:05 +0100

    nieuwe opdrachten

---
+++ SOM/classobj.py
@@ -0,0 +1,20 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length*self.width
+
+    def center(self):
+        return self.length*0.5, self.width*0.5
+
+        
+
+
+
+
+
+
+    
+
